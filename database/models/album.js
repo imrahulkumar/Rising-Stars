16cc66fc583cd4f6ffdd7de22e74917f24236359
Rahul Kumar <rahulkumar151995@gmail.com>
2019-04-18 23:45:43 +0530

    implemented basic queries

--- database/models/album.js
+++ database/models/album.js
@@ -1,3 +1,15 @@
-// Todo: create Album Schema
-//dfdsfdsdsfsdfds
+import { Schema } from "mongoose";
 
+// Todo: create Album Schema
+const mongoose = require('mongoose');
+
+const AlbumSchema = new Schema({
+    title:String,
+    date:Date,
+    copiesSolid:Number,
+    numberTracks:Number,
+    image:String,
+    revenue:Number
+})
+
+module.exports = AlbumSchema;
