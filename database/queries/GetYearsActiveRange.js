9d3b2e0ca22f9f38c803843fa4be4cf4a22e5f02
Rahul Kumar <rahulkumar151995@gmail.com>
2019-04-20 21:03:24 +0530

    complete

--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,20 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+    const minQuery = Artist
+    .find({})
+    .sort({ yearActive: 1 })
+    .limit(1)
+    .then(artists => artists[0].yearActive);
+
+const maxQuery = Artist
+    .find({})
+    .sort({ yearActive: -1 })
+    .limit(1)
+    .then(artists => artists[0].yearActive);
+
+    return Promise.all([minQuery,maxQuery])
+    .then((result)=>{
+        return {min : result[0],max:result[1]}
+    });
 };
--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,4 +9,45 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+   
+    // const sortOrder = {};
+    // sortOrder[sortProperty] = 1;
+   
+  const query =  Artist.find(buildQuery(criteria))
+    .sort({[sortProperty]:1})
+    .skip(offset)
+    .limit(limit);
+
+    return Promise.all([query,Artist.count()])
+    .then((results)=>{
+        return {
+            all:results[0],
+            count:results[1],
+            offset:offset,
+            limit:limit
+        };
+    });
 };
+
+const buildQuery = (criteria) =>{
+    const query ={};
+
+if(criteria.name){
+    criteria.$text = {$search: criteria.name};
+}
+
+    if(criteria.age){
+        query.age = {
+            $gte:criteria.age.min,
+            $lte:criteria.age.max
+        };
+    }
+    if(criteria.yearActive){
+        query.yearActive = {
+            $gte:criteria.yearsActive.min,
+            $lte:criteria.yearsActive.max
+        };
+    }
+
+    return query
+}
